da02edbfe84f8f51f92e53e79aad12c1df48187c
ddoemonn <ozergokalpsezer@gmail.com>
2024-07-02 18:12:02 +0300

    ADD: layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
-import Image from 'next/image';
-
-import { Dosis } from 'next/font/google';
-
 import { Navbar } from '@/components';
-import image from '@/public/image.png';
-const inter = Dosis({ subsets: ['latin'] });
+
+import Layout from './layout';
 
 export default function Home() {
   return (
-    <main
-      className={` min-h-screen flex flex-col items-center ${inter.className} bg-gradient-to-r from-indigo-300 from-10% via-sky-300 via-50% to-emerald-300 to-90%`}
-    >
-      <Navbar />
+    <Layout>
       <section className="flex flex-col justify-center items-center flex-1 w-1/2">
         <input
           placeholder="Search for your favorite media..."
@@ -24,6 +17,6 @@ export default function Home() {
           <strong className="italic"> music </strong>. Connect with like-minded enthusiasts, share your reviews, and discover new favorites together!
         </p>
       </section>
-    </main>
+    </Layout>
   );
 }
